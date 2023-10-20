88eb76c5a12e08e7b10489ae714f08dd8bb2ad4e
Mohsen Rzna <ash.wxrz@hotmail.com>
2023-10-20 23:03:57 +0200

    Moved/removed the files

--- androidApp/src/androidMain/kotlin/com/client/news/ui/data/repository/MainRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.client.news.ui.data.repository
-
-import com.client.news.ui.data.model.TopHeadlinesResponse
-import kotlinx.coroutines.flow.Flow
-
-interface MainRepository {
-
-    suspend fun getTopHeadlines(): Flow<TopHeadlinesResponse>
-}
